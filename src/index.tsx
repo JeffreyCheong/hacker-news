b157fe2791bcd8c8107622b3d675ddbb33579606
jeffrey cheong <jeffrey.cheong@pavcsg.com>
2021-02-10 18:29:29 +0800

    base commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Helmet } from 'react-helmet';
 
+// #ff6600 orange header
+// 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <meta name="referrer" content="origin" />
+      <title>New Links | Hacker News</title>
+      <meta name="description" content="Hacker News is a social news website focusing on computer science and entrepreneurship" />
+    </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
